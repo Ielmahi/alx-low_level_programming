117cf61f45119d152c9b6078b53179c168c89ba4
Ielmahi <vodkailias@gmail.com>
2023-09-07 09:27:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle ";)
+#include <stdio.h>
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
