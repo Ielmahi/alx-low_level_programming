62531cde2f0238643c8adfb0a4c2711d8fba52cd
Ielmahi <vodkailias@gmail.com>
2023-09-07 09:38:43 -0700

    Hello, puta

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
