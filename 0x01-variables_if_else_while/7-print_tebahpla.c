da7db02ad5d6efa2711679396a4c9865dbf3af5c
Ielmahi <vodkailias@gmail.com>
2023-09-08 12:06:35 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+
+	for(i = 'z';i >= 'a';i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
