9ba0946aabc4aa12a2c1cab51594e001c6d10836
Ielmahi <vodkailias@gmail.com>
2023-09-12 02:57:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry Block
+  *Description: prints _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
