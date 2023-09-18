62ffbea68eca4cc11da8aa6f96470fb672555330
Ielmahi <140087474+Ielmahi@users.noreply.github.com>
2023-09-18 16:54:26 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts - prints a string, followed by a new line to stdout.
  * @str: input string.
@@ -6,10 +7,18 @@
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int count = 0;
+
+	while (count >= 0)
 	{
-		_putchar(*str + 0);
-		str++;
+		if (str[count] != '\0')
+		{
+			_putchar(str[count]);
+			count++;
+		} else
+		{
+			count = -1;
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
