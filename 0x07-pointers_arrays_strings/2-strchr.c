e4d5e567c3927fa2b0d2bb2547b7e461ee20b474
Ielmahi <vodkailias@gmail.com>
2023-09-25 14:52:05 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
+	for (; s[i] != '\0'; i++)
+		if (s[i] == c)
 			return (s + i);
-	if (*(s + i) == c)
+	if (s[i] == c)
 		return (s + i);
 	return ('\0');
 }
