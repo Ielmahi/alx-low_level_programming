55b5bcd26f311b57fc83b8979ed70c4742e913fc
Ielmahi <vodkailias@gmail.com>
2023-09-25 11:23:56 +0200

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,6 +11,6 @@ char *_memset(char *s, char b, unsigned int n)
 	for (unsigned int i = 0; i < n; i++)
 		s[i] = b;
 	for (unsigned int i = 0; i < n ; i++)
-		printf("%c", s[i]);
-	printf("\n");
+		_putchar(s[i]);
+	_putchar('\n');
 }
