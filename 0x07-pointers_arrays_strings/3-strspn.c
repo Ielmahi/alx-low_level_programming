641be360c7bc4ea2fbc5ed6731003987fc9d40f9
Ielmahi <vodkailias@gmail.com>
2023-09-25 15:32:13 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		fro (j = 0; accept[j] != s[i]; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);
