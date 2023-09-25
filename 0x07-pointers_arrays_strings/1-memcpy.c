7fc5fc13876e53571839812709f223690dbfaeea
Ielmahi <vodkailias@gmail.com>
2023-09-25 12:36:43 +0200

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,9 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i ;
 
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
 	return (dest);
 }
