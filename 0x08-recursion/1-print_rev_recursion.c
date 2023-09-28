bca5f46194ea7058c10bbbda821a0c089ac46baf
Ielmahi <vodkailias@gmail.com>
2023-09-28 12:11:05 +0200

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
-  * _print_rev_recursion - Prints a string, followed by a new line,in reverse order
-  * @s : The string to be printed in reverse order
-  *
-  * This function prints each character of the given string @s recursively,in reverse order
-  * until it reaches the null terminator, at which point it prints a new line.
-  *
-  * Return: void.
-  */
-
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string to print
+ * Return: void.
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
