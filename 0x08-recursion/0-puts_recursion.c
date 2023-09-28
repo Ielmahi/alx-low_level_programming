b05e29badd346a72ad1e0aa960decfc8a67831b6
Ielmahi <vodkailias@gmail.com>
2023-09-28 11:34:29 +0200

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
-/**_puts_recursion : function that prints a string, followed by a new line.
-  * @s : string
-  * return : void .
-  */
+/**
+ * _puts_recursion - Prints a string, followed by a new line, recursively.
+ * @s: The string to be printed.
+ *
+ * This function prints each character of the given string @s recursively,
+ * until it reaches the null terminator, at which point it prints a new line.
+ *
+ * Return: void.
+ */
+
 
 void _puts_recursion(char *s)
 {
