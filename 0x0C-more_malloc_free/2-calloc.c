35f88fe88e74add0a436b371e3aa5a2df2f5d904
Ielmahi <vodkailias@gmail.com>
2023-10-06 12:43:42 +0200

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+		if (ptr == NULL)
+			return (NULL);
+	return (ptr);
+}
