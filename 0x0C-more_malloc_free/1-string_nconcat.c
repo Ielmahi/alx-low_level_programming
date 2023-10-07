961a7738743293a26fcdea4539022e8368047982
Ielmahi <vodkailias@gmail.com>
2023-10-07 11:34:19 +0200

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
  * *string_nconcat - concatenates n bytes of a string to another string
@@ -10,37 +9,37 @@
  *
  * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0;
-	char *result;
-	unsigned int i, j;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	if (s2 != NULL)
-	{
-		len2 = strlen(s2);
-		if (n >= len2)
-		{
-			n = len2;
-		}
-	}
-	result = malloc(len1 + n + 1);
-	if (result == NULL)
-	{
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-		result[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-		result[i + j] = s2[j];
-	}
-	result[i + j] = '\0';
 
-	return (result);
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
