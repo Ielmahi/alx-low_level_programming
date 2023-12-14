65d9f77666d7c53a951fc7908c970ec04df6ac04
Ielmahi <vodkailias@gmail.com>
2023-12-14 13:28:08 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b)
 	{
-		if (*b ! '0' && *b != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 		num = num * 2 + (*b++ - '0');
 	}
